26b949cbd3f4688f6d8b7ead6ac2ca7820965081
John <ADJordan305@hotmail.com>
2023-11-22 01:12:07 -0600

    .

--- src/commands/informational/user.ts
+++ src/commands/informational/user.ts
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder, time } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('userinfo')
+    .setName('user')
     .setDescription("Returns The Tageted Users Information")
     .addUserOption((option) =>
       option.setName("target").setDescription('the targeted user').setRequired(false)
@@ -13,19 +13,25 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setColor('Blue')
       .setTitle(`${target?.username}'s Info`)
-      .setDescription(`${target?.id}`)
+      .setDescription(`||${target?.id}||`)
       .addFields([
         {
-          name: 'User mention',
+          name: 'User Mention',
           value: `<@${target.id}>`,
+          inline: true
+        },
+        {
+          name: 'User Display',
+          value: `${target.globalName}`,
           inline: true
         },
         { 
           name: 'Account Created',
           value: time(target.createdAt),
-          inline: true
+          inline: false
         }
       ])
+      .setImage(target.displayAvatarURL())
       .setTimestamp(Date.now())
       .setFooter({
         text: `Requested By: ${interaction.user.username}`
